80338efd5f81edaf19b14c92d80765e34b0aedf7
XMisterFry <syedhasanali931@gmail.com>
2025-04-25 01:50:03 +0000

    routing-done

---
+++ routes/courses.js
@@ -0,0 +1,17 @@
+let {Router} = require ("express");
+
+let courseRouter = Router();
+
+
+
+
+
+courseRouter.post ('/purchase', (req,res)=> {
+    
+})
+
+courseRouter.get ('/view', (req,res)=> {
+    
+})
+
+module.exports = {courseRouter}
---
+++ routes/user.js
@@ -0,0 +1,18 @@
+let {Router} = require ("express");
+
+let userRouter = Router();
+
+
+userRouter.post ('/signup', (req,res)=> {
+
+})
+
+userRouter.post ('/signin', (req,res)=> {
+    
+})
+
+userRouter.get ('/my-purchases', (req,res)=> {
+    
+})
+
+module.exports={userRouter}
