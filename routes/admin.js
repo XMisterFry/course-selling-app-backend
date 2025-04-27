ad323a442dda2202acc91e8f5478c5f60fbffcda
XMisterFry <syedhasanali931@gmail.com>
2025-04-27 02:18:19 +0000

    auth middleware added

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,8 @@
 let {Router} = require ("express");
 const  {adminModel} = require  ("../db")
 let adminRouter = Router();
+let jwt = require ('jsonwebtoken')
+const JWT_SECRET = "qwerTYuiop"
 
 
 adminRouter.post ('/signup', (req,res)=> {
@@ -10,14 +12,34 @@ adminRouter.post ('/signup', (req,res)=> {
 adminRouter.post ('/signin', (req,res)=> {
     
 })
-adminRouter.post ('/create-course', (req,res)=> {
+
+
+
+let auth = (req,res,next) => {
+const token = req.headers.token
+const decodedData = jwt.verify(token, JWT_SECRET)
+if (decodedData) {
+req.user.id = decodedData.id
+next ();
+}
+
+else {
+    res.status (403).json({
+        msg : "You are not signed in"
+    });
+}
+}
+
+
+
+adminRouter.post ('/create-course', auth , (req,res)=> {
     
 }) 
 
-adminRouter.put ('/update-course', (req,res)=> {
+adminRouter.put ('/update-course', auth,  (req,res)=> {
     
 })
-adminRouter.get ('/my-dashboard', (req,res)=> {
+adminRouter.get ('/my-dashboard', auth , (req,res)=> {
     
 })
 
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,8 @@
 let {Router} = require ("express");
-const  {userModel, purchasesModel} = require ("../db")
+const  {userModel, purchasesModel} = require ("../db");
 let userRouter = Router();
+let jwt = require ('jsonwebtoken')
+const JWT_SECRET = "qwerTYuiop"
 
 
 userRouter.post ('/signup', (req,res)=> {
@@ -11,7 +13,23 @@ userRouter.post ('/signin', (req,res)=> {
     
 })
 
-userRouter.get ('/my-purchases', (req,res)=> {
+let auth = (req,res,next) => {
+const token = req.headers.token
+const decodedData = jwt.verify(token, JWT_SECRET)
+if (decodedData) {
+req.user.id = decodedData.id
+next ();
+}
+
+else {
+    res.status (403).json({
+        msg : "You are not signed in"
+    });
+}
+}
+
+
+userRouter.get ('/my-purchases', auth, (req,res)=> {
     
 
 
